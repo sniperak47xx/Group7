9c4761bb7a2fd9cb9c560997b1e9296af3dabdea
Martin <martin@wirksomhed.dk>
2019-03-28 12:47:45 +0100

    Fix

--- AsteroidsOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
+++ AsteroidsOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
@@ -3,7 +3,7 @@ package dk.sdu.mmmi.cbse.common.data;
 import dk.sdu.mmmi.cbse.common.data.entityparts.EntityPart;
 import java.io.Serializable;
 import java.util.Collection;
-//import java.util.Map;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,23 +13,23 @@ public class Entity implements Serializable {
     private float[] shapeX = new float[4];
     private float[] shapeY = new float[4];
     private float radius;
-//    private Map<Class, EntityPart> parts;
+    private Map<Class, EntityPart> parts;
     
-//    public Entity() {
-//        parts = new ConcurrentHashMap<>();
-//    }
-//    
-//    public void add(EntityPart part) {
-//        parts.put(part.getClass(), part);
-//    }
-//    
-//    public void remove(Class partClass) {
-//        parts.remove(partClass);
-//    }
+    public Entity() {
+        parts = new ConcurrentHashMap<>();
+    }
     
-//    public <E extends EntityPart> E getPart(Class partClass) {
-//        return (E) parts.get(partClass);
-//    }
+    public void add(EntityPart part) {
+        parts.put(part.getClass(), part);
+    }
+    
+    public void remove(Class partClass) {
+        parts.remove(partClass);
+    }
+    
+    public <E extends EntityPart> E getPart(Class partClass) {
+        return (E) parts.get(partClass);
+    }
     
     public void setRadius(float r){
         this.radius = r;
