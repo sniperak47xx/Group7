79da132f3b895788978ccc6b2ae2505eda21c107
Borgar Bordoy <bobor16@student.sdu.dk>
2019-03-28 12:55:16 +0100

    Added modules for Map

---
+++ AsteroidsOSGi/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/Activator.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.cbse.osgimap;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+        // TODO add activation code here
+        System.out.println("MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP");
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // TODO add deactivation code here
+    }
+
+}
